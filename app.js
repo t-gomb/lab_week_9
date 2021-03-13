acef1c54ab806a15c36ecafd8cc6ec37d86e2c22
t-gomb <trentgombas@gmail.com>
2021-03-12 20:35:22 -0800

    only_commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let x = 2;
+let y = 3;
+str = "stock"
+boo === "true"
+if ((x === y)) {
+    console.log('They match');
+}
+else {
+    console.log('no luck');
+}
+
+var str = "stock";
+for (var i = 0; i < str.length; i++) {
+  console.info(str[i]);
+}
